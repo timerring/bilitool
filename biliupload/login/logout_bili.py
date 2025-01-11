6c7182d8fecb1b2fae4d30b4d6a64ddec893d0a1
John Howe <89397553+timerring@users.noreply.github.com>
2025-01-11 19:07:53 +0800

    feat: add logout module (#8)

---
+++ biliupload/login/logout_bili.py
@@ -0,0 +1,37 @@
+import http.client
+import urllib.parse
+from biliupload.ioer import ioer
+import json
+
+
+# The requests lib here was suspended by the official, so use http.client to make the request
+class Logout(object):
+    def __init__(self):
+        self.config = ioer().get_config()
+
+    def logout_bili(self):
+        host = 'passport.bilibili.com'
+        path = '/login/exit/v2'
+
+        headers = {
+            'Cookie': f'DedeUserID={self.config["cookies"]["DedeUserID"]}; bili_jct={self.config["cookies"]["bili_jct"]}; SESSDATA={self.config["cookies"]["SESSDATA"]}',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        data = {
+            'biliCSRF': self.config["cookies"]["bili_jct"]
+        }
+        encoded_data = urllib.parse.urlencode(data)
+        connection = http.client.HTTPSConnection(host)
+
+        connection.request('POST', path, body=encoded_data, headers=headers)
+
+        response = connection.getresponse()
+        response_json = json.loads(response.read().decode('utf-8'))
+        if response_json['code'] == 0:
+            print("Logout successfully, the cookie has expired")
+            ioer().reset_cookies()
+        else:
+            print("Logout failed, check the info:")
+            print(response_json)
+        connection.close()
