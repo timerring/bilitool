5cd881c97e3806cc0584e0d86995dc106db42624
John Howe <89397553+timerring@users.noreply.github.com>
2025-01-14 18:23:19 +0800

    refactor: optimize the unittest preformance (#19)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,9 +2,10 @@
 
 import unittest
 from bilitool.utils.get_ip_info import IPInfo
+from bilitool.utils.check_format import CheckFormat
+
 
 class TestIPInfo(unittest.TestCase):
     def test_get_ip_address(self):
         self.assertEqual(IPInfo.get_ip_address('12.12.12.12'), 
         ('12.12.12.12', 'att.com', '美国阿拉斯加州安克雷奇', '61.108841,-149.373145'))
- 
