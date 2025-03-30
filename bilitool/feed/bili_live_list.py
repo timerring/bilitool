ad55d471a25f078c101d68b173c171a31438ecf4
John Howe <89397553+timerring@users.noreply.github.com>
2025-03-30 20:54:44 +0800

    feat: add live info

---
+++ bilitool/feed/bili_live_list.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2025 bilitool
+
+import requests
+
+class BiliLiveList:
+    def __init__(self, headers):
+        self.headers = headers
+
+    def get_live_info(self, room) -> dict:
+        """Get the live info of the room"""
+
+        url = "https://api.live.bilibili.com/room/v1/Room/get_info?room_id={room}".format(
+            room=room
+        )
+        response = requests.get(url=url, headers=self.headers)
+        if response.status_code != 200:
+            raise Exception('HTTP ERROR')
+        response_json = response.json().get("data")
+        if response.json().get("code") != 0:
+            raise Exception(response.json().get("message"))
+        return response_json
+
--- tests/test_feed.py
+++ tests/test_feed.py
@@ -2,8 +2,14 @@
 
 import unittest
 from bilitool.feed.bili_video_list import BiliVideoList
+from bilitool.feed.bili_live_list import BiliLiveList
 
 class TestBiliList(unittest.TestCase):
+    def setUp(self):
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 Chrome/63.0.3239.108"
+        }
+
     def test_get_bili_video_list(self):
         bili = BiliVideoList()
         bili.print_video_list_info(bili.get_bili_video_list(50, 'not_pubed'))
@@ -11,3 +17,7 @@ class TestBiliList(unittest.TestCase):
     def test_print_video_info_via_bvid(self):
         bili = BiliVideoList()
         bili.print_video_info_via_bvid('BV1pCr6YcEgD')
+
+    def test_get_live_info(self):
+        bili = BiliLiveList(self.headers)
+        print(bili.get_live_info(25538755))
